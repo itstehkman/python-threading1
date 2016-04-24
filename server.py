54d78994dacfe98f1ef5ee5e5be5381310eba099
itstehkman <itstehkman@gmail.com>
2016-04-24 16:06:22 -0700

    True parallelism with ProcessPoolExecutor

--- server.py
+++ server.py
@@ -2,6 +2,9 @@ from fib import fib
 
 from socket import *
 from threading import Thread
+from concurrent.futures import ProcessPoolExecutor as Pool
+
+pool = Pool(4)  #use 4 processes (true parallelism)
 
 def fib_server(address):
 	sock = socket(AF_INET, SOCK_STREAM)
@@ -11,20 +14,21 @@ def fib_server(address):
 	while True:
 		clientsock, addr = sock.accept()  #blocking
 		print "Connection", addr
-		t = Thread(target=fib_handler, args=(clientsock,))
+		t = Thread(target=fib_handler, args=(clientsock,))  
 		#daemon thread = we don't need to tell it to exit when this python program finishes
 		t.daemon=True 
 		t.start()
 
 def fib_handler(clientsock):
 	while True:
-		req = clientsock.recv(100)
+		req = clientsock.recv(100)  #blocking
 		if not req: 
 			break
 		n = int(req)  #requests are strings. this will break if int() doesn't work
-		result = fib(n)
+		future = pool.submit(fib, n)
+		result = future.result()  #blocking
 		resp = str(result).encode('ascii') + b'\n'  #b for bytes
-		clientsock.send(resp)
+		clientsock.send(resp)  #potentially blocking
 	print "Closed"
 
 #localhost port 25000
